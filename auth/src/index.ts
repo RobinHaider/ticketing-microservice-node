88dc1850fedcf0fedd881267cc3070b87d67ddd1
Robin Haider <robinhaider69@gmail.com>
2022-08-30 08:30:57 +0600

    kubectl - ingress service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,11 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send("Hello Robin!!");
+})
+
 app.listen(3000, () => {
-    console.log("Listening on port 3000...");
+    console.log("Listening on port 3000....");
     
 })
