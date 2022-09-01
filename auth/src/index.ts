13a43d0c016942f3c6b96de5b369ba5171cccea9
Robin Haider <robinhaider69@gmail.com>
2022-09-01 14:33:42 +0600

    mongoose added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,14 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from './routes/signup';
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -19,13 +20,33 @@ app.use(signupRouter);
 
 // not found route
 app.all('*', async (req, res, next) => {
-    throw new NotFoundError();
-})
+  throw new NotFoundError();
+});
 
 // add error handler middleware
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000..");
+
+// mongo db connect
+// mongodo://serviceName:port/databasename
+// if database not exist, it will create
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log("Connected to MongoDB");
     
-})
+  } catch (error) {
+    console.error(error);
+  }
+
+  // start express
+  app.listen(3000, () => {
+    console.log('Listening on port 3000..');
+  });
+};
+
+
+// start app...
+start();
+
+
