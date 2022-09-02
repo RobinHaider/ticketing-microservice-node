f000a8e7b3c8d2d81264231f245a2a3b171cfabb
Robin Haider <robinhaider69@gmail.com>
2022-09-02 09:52:10 +0600

    User Model Update

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,7 +1,19 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // properties required to create a new user
 interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+// User Model
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// User Document after create
+interface UserDoc extends mongoose.Document{
     email: string;
     password: string;
 }
@@ -17,10 +29,23 @@ const userSchma = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model('User', userSchma);
+// before save to mongo hash password
+userSchma.pre('save', async function(done) {
+    if(this.isModified('password')){
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
 
-const buildUser = (attrs: UserAttrs) => {
+    done();
+})
+
+// add build method to user
+userSchma.statics.build = (attrs: UserAttrs) => {
     return new User(attrs);
 }
 
-export {User, buildUser};
+const User = mongoose.model<UserDoc, UserModel>('User', userSchma);
+
+
+
+export {User};
