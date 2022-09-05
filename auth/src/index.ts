7a58a7ede88ec75d6763dcc2914edf73a813f970
Robin Haider <robinhaider69@gmail.com>
2022-09-06 05:40:01 +0600

    test setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,54 +1,18 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session'; 
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    // make it secure true so that is only works on https
-    // secure: true
-  })
-)
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// not found route
-app.all('*', async (req, res, next) => {
-  throw new NotFoundError();
-});
-
-// add error handler middleware
-app.use(errorHandler);
-
+import { app } from './app';
 
 // mongo db connect
 // mongodo://serviceName:port/databasename
 // if database not exist, it will create
 const start = async () => {
   // jwt env check
-  if(!process.env.JWT_KEY){
+  if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
-  
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-    console.log("Connected to MongoDB");
-    
+    console.log('Connected to MongoDB');
   } catch (error) {
     console.error(error);
   }
@@ -59,8 +23,5 @@ const start = async () => {
   });
 };
 
-
 // start app...
 start();
-
-
