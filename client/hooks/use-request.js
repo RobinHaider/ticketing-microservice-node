ebf84451c51433aec7cac62f8b40c92fefb3890e
Robin Haider <robinhaider69@gmail.com>
2022-09-07 11:55:21 +0600

    use request hooks

---
+++ client/hooks/use-request.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+export default ({ url, method, body, onSuccess }) => {
+  const [errors, setErrors] = useState(null);
+
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, body);
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+      console.log(response.data);
+    } catch (error) {
+      setErrors(
+        <div className='alert alert-danger'>
+          <h4>Oooops</h4>
+          <ul className='my-0'>
+            {error.response.data.errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return { doRequest, errors };
+};
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,23 +1,24 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
+  const {doRequest, errors} = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+        email, password
+    },
+    onSuccess: () => Router.push('/')
+  });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email,
-        password,
-      });
-      console.log(response.data);
-    } catch (error) {
-      setErrors(error.response.data.errors);
-    }
+    doRequest();
+
   };
 
   return (
@@ -40,16 +41,7 @@ export default () => {
           className='form-control'
         />
       </div>
-      {errors.length > 0 && (
-        <div className='alert alert-danger'>
-          <h4>Oooops</h4>
-          <ul className='my-0'>
-            {errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {errors}
       <button type='submit' className='btn btn-primary mt-2'>
         Sign Up
       </button>
