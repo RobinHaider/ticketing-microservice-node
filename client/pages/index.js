bc7706bde7449af9f59d0944eab32ee3ce3bd3ef
Robin Haider <robinhaider69@gmail.com>
2022-09-08 13:41:20 +0600

    index update

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,15 @@
-export default () => {
+import axios from 'axios';
+
+
+const LandingPage = ({currentUser}) => {
+    console.log(currentUser);
+    axios.get('/api/users/currentuser');
     return <h1>Landing Page</h1>
 }
+
+// LandingPage.getInitialProps = async () => {
+//     const response = await axios.get('/api/users/currnetuser');
+//     return response.data;
+// }
+
+export default LandingPage;
