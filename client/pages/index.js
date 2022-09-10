35fabcfab2de29844d794cd27d1efb7d0aef9c29
Robin Haider <robinhaider69@gmail.com>
2022-09-10 11:37:33 +0600

    header component

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,6 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log('user', currentUser);
   return <h1>{currentUser ? 'You are signed in' : 'You are not signed in'}</h1>;
 };
 
