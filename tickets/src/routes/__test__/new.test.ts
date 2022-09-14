aa69f9c2214c41ec736b55368c56a9359f57be59
Robin Haider <robinhaider69@gmail.com>
2022-09-14 10:24:51 +0600

    create ticket route - save to db

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { getCookie } from '../../helpers/signin-test-helper';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler to /api/tickets for post request', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -60,10 +61,20 @@ it('return an error if an invalid price is provided', async () => {
 
 it('create a ticket with valid input', async () => {
   // add in a check to make sure the ticket is saved
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  const title = 'newtitle';
+  const price = 20;
 
   await request(app)
     .post('/api/tickets')
     .set('Cookie', getCookie())
-    .send({ title: 'newtitle', price: 20 })
+    .send({ title, price })
     .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual(title);
+  expect(tickets[0].price).toEqual(price);
 });
