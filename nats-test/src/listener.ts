b6fae2088d7de747de6b7d63165cf34aa35895f1
Robin Haider <robinhaider69@gmail.com>
2022-10-15 12:23:54 +0600

    on close event

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,15 +1,26 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
+  stan.on('close', () => {
+    console.log('NATS connection close');
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group',
+    options
+  );
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
@@ -17,5 +28,10 @@ stan.on('connect', () => {
     if (typeof data === 'string') {
       console.log(`Received Event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
