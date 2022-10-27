2a4abcb4a84538acae8700a2b36acbe18820d315
Robin Haider <robinhaider69@gmail.com>
2022-10-27 13:18:40 +0600

    nats environment variable

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -15,9 +15,23 @@ const start = async () => {
     throw new Error('MONGO_URI must be defined');
   }
 
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
   try {
     // nats connect
-    await natsWrapper.connect('ticketing', 'abcd', 'http://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
 
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
