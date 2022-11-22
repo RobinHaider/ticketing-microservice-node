daffbe11eaf44fdc6a7a2117b6e30fe7192bc397
Robin Haider <robinhaider69@gmail.com>
2022-11-22 09:30:22 +0600

    ticket created listener in order serivice

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,22 @@
+import {
+  Listener,
+  Subjects,
+  TicketCreatedEvent,
+} from '@robinhaider3/ticketing-common';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    const { title, price } = data;
+
+    const ticket = Ticket.build({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
