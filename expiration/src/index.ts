326a8064c6c3abe6e7ccf5816b6885e1aee7ac7b
Robin Haider <robinhaider69@gmail.com>
2023-01-12 11:26:46 +0600

    expiration service setup

---
+++ expiration/src/index.ts
@@ -0,0 +1,37 @@
+import { natsWrapper } from './nats-wrapper';
+
+// mongo db connect
+// mongodo://serviceName:port/databasename
+// if database not exist, it will create
+const start = async () => {
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
+  try {
+    // nats connect
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// start app...
+start();
