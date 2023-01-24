b3148323df1466be37bd9e040841b0722df5e48b
Robin Haider <robinhaider69@gmail.com>
2023-01-24 20:34:32 +0600

    client - order and ticket pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,7 +6,9 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <div className='container'>
+        <Component currentUser={currentUser} {...pageProps} />
+      </div>
     </div>
   );
 };
@@ -15,15 +17,18 @@ AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
   const { data } = await client.get('/api/users/currentuser');
 
-  // call getinitialProps for page component
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx,
+      client,
+      data.currentUser
+    );
   }
 
   return {
     pageProps,
-    ...data
+    ...data,
   };
 };
 
